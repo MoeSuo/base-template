076926e5cea198f0a39646a4ea13700604d5e8b1
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-10-20 12:21:14 +0300

    test deployment-3

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -17,7 +17,7 @@ export const Heading = () => {
         Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Notely App</span>
       </h1>
       {/* <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jotion is the connected workspace where <br />
+        Notely is the connected workspace where <br />
         better, faster work happens.
       </h3> */}
       {isLoading && (
