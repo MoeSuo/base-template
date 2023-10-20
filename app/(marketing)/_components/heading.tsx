6661cecc0bbbc5cb3b9ec08deb433d2746f98930
Mohamed Sallam <33803711+MoeSuo@users.noreply.github.com>
2023-10-20 11:41:39 +0300

    Update heading.tsx

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -14,7 +14,7 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Notely App</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         Jotion is the connected workspace where <br />
@@ -28,7 +28,7 @@ export const Heading = () => {
       {isAuthenticated && !isLoading && (
         <Button asChild>
           <Link href="/documents">
-            Enter Jotion
+            Create New Note
             <ArrowRight className="h-4 w-4 ml-2" />
           </Link>
         </Button>
@@ -36,7 +36,7 @@ export const Heading = () => {
       {!isAuthenticated && !isLoading && (
         <SignInButton mode="modal">
           <Button>
-            Get Jotion free
+            Get Notely free
             <ArrowRight className="h-4 w-4 ml-2" />
           </Button>
         </SignInButton>
