dd35464887ef9e103f699a60b4a2ae812dfaa110
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-10-20 11:15:56 +0300

    test deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,59 @@
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Note App",
-  description: "Create a modern Notes",
+  title: 'Jotion',
+  description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
-
       {
         media: "(prefers-color-scheme: light)",
-        url: "/light.svg",
-        href: "/light.svg",
+        url: "/logo.svg",
+        href: "/logo.svg",
       },
       {
         media: "(prefers-color-scheme: dark)",
-        url: "/dark_icon.svg",
-        href: "/dark_icon.svg",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
       }
-    ],
-  },
-};
+    ]
+  }
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-      <ConvexClientProvider>
-
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          // disableTransitionOnChange
-          storageKey="notely-theme"
-          >
-          {children}
-        </ThemeProvider>
-          </ConvexClientProvider>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="jotion-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
       </body>
     </html>
-  );
+  )
 }
